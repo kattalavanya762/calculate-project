65da0eccac24116a4b0534a36450d8464d962f77
kattalavanya762 <kattalavanya762@gmail.com>
2026-05-18 16:23:47 +0530

    Implement basic login functionality

---
+++ login system.py
@@ -0,0 +1,6 @@
+username=input("Enter the username:")
+password=input("Enter the password:")
+if username=="admin" and password=="12345":
+    print("login successful")
+else:
+    print("invalid password and username")
